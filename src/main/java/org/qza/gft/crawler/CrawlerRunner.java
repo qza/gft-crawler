2ae65c94d092b41dc3142c8e490fa5c9bf29e01b
qza <kokovic.zoran@gmail.com>
2013-04-07 01:07:29 -0700

    Added main runner and configured maven exec plugin

---
+++ src/main/java/org/qza/gft/crawler/CrawlerRunner.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.qza.gft.crawler;
+
+/**
+ * @author qza
+ * 
+ * Initiates crawling process
+ *
+ */
+public class CrawlerRunner {
+
+	/**
+	 * Created new CrawlerRunner instance
+	 */
+	public CrawlerRunner() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void start() {
+		// TODO 
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CrawlerRunner runner = new CrawlerRunner();
+		runner.start();
+	}
+
+}
